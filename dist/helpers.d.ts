14ba2356b4f64d1b60921b65e7fef29e41d5852b
Dmitry Zakharov <dzakh.dev@gmail.com>
2021-12-28 02:24:14 +0300

    Remove the dist folder from the repository

--- dist/helpers.d.ts
+++
@@ -1,5 +0,0 @@
-import { useLayoutEffect } from 'react';
-export declare function isUndefined(value: any): boolean;
-export declare function getScrollX(): number;
-export declare function getScrollY(): number;
-export declare const useIsomorphicLayoutEffect: typeof useLayoutEffect;
